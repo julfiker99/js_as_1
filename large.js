809e59240c91b5b805057262a65fe02e37414725
SKM Julfiker Ali <136487960+julfiker99@users.noreply.github.com>
2025-01-13 13:03:55 +0600

    Update large.js

--- large.js
+++ large.js
@@ -1,1 +1,8 @@
+var numbers = [1, 2, 3, 3, 4, 4, 5, 6, 7, 8, 9, 10];
+
+var uniqueNumbers = [...new Set(numbers)];
+console.log("Unique numbers:", uniqueNumbers);
+
+var largestNumber = Math.max(...uniqueNumbers);
+console.log("Largest number:", largestNumber);
 
